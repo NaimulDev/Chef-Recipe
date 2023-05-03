2e3d90e1d129f0faec7c0841834200776067e8b1
Nimul99 <htnimul99@gmail.com>
2023-05-03 21:38:26 +0600

    added banner image,  extra products  section and resonsive small and large device

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,45 +1,20 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import cover from "./assets/images/banner.png";
+
 import sweets from "./assets/images/foods/sweets.jpg";
 import burgers from "./assets/images/foods/burger.jpg";
 import drinks from "./assets/images/foods/drinks.jpg";
 import pizza from "./assets/images/foods/pizza.jpg";
+import cover1 from "./assets/images/banner/banner-1.jpg";
+// import cover2 from "./assets/images/banner/banner-1.jpg";
+// import cover3 from "./assets/images/banner/banner-1.jpg";
 
 const Home = () => {
   return (
     <>
-      {/* <div className="flex flex-col md:flex-row justify-center items-center md:pl-24 bg-gray-100">
-        <div className="md:w-1/2 px-4 py-8 md:py-16">
-          <h4 className="text-gray-600 mt-8 md:mt-0 md:text-xl lg:text-2xl">
-            100% Natural
-          </h4>
-          <h1 className="text-3xl md:text-6xl font-bold mb-4 text-slate-800 mt-4 md:mt-8 lg:mt-16">
-            Chocolate Chip Cream
-          </h1>
-          <p className="mb-8 md:mb-16 text-base md:text-xl">
-            Our chocolate chip cookies are made with all natural ingredients and
-            baked fresh daily. We use the finest chocolate chips and cream to
-            create a deliciously soft and chewy treat thats perfect for any
-            occasion.
-          </p>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-6 rounded-full">
-            Book A Table
-          </button>
-        </div>
-
-        <div className="md:w-1/2">
-          <img
-            src={cover}
-            alt=""
-            className="object-cover w-full h-full md:rounded-r-lg"
-          />
-        </div>
-      </div> */}
-
       <div className="relative h-screen">
         <img
-          src={cover}
+          src={cover1}
           alt=""
           className="object-cover w-full h-full absolute top-0 left-0 z-0"
         />
